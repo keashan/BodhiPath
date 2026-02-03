7726b22fa13db3a3a179b5db87279820bae56fb3
Keashan Jayaweera <keashanjayaweera@gmail.com>
2026-02-03 11:46:18 +0530

    build error fixed

--- components/DailyDropsView.tsx
+++ components/DailyDropsView.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { ChevronLeft, Share2, Quote, History, Calendar, Download, Loader2 } from 'lucide-react';
 import { Language, DailyDrop } from '../types';
 import { UI_TEXT } from '../constants';
-import { toPng } from 'html-to-image';
+import { toPng } from 'https://esm.sh/html-to-image@1.11.11';
 
 interface DailyDropsViewProps {
   language: Language;
